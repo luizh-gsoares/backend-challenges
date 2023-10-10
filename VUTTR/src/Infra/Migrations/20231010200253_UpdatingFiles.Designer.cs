a87d2ad4221f4036e4db2535e41405ce705721d2
Luiz Henrique Soares <luizhenriquesoares18@hotmail.com>
2023-10-10 17:03:57 -0300

    Adding migrations

--- VUTTR/src/Infra/Migrations/20231010200253_UpdatingFiles.Designer.cs
+++ VUTTR/src/Infra/Migrations/20231010200253_UpdatingFiles.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using VUTTR.Infra.Data;
 
@@ -10,9 +11,11 @@ using VUTTR.Infra.Data;
 namespace VUTTR.Infra.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231010200253_UpdatingFiles")]
+    partial class UpdatingFiles
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ VUTTR/src/Infra/Migrations/20231010200253_UpdatingFiles.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VUTTR.Infra.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatingFiles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tags_Tools_ToolId",
+                table: "Tags");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Tags_ToolId",
+                table: "Tags");
+
+            migrationBuilder.DropColumn(
+                name: "ToolId",
+                table: "Tags");
+
+            migrationBuilder.CreateTable(
+                name: "TagTool",
+                columns: table => new
+                {
+                    TagsId = table.Column<int>(type: "int", nullable: false),
+                    ToolsId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TagTool", x => new { x.TagsId, x.ToolsId });
+                    table.ForeignKey(
+                        name: "FK_TagTool_Tags_TagsId",
+                        column: x => x.TagsId,
+                        principalTable: "Tags",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_TagTool_Tools_ToolsId",
+                        column: x => x.ToolsId,
+                        principalTable: "Tools",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ToolTags",
+                columns: table => new
+                {
+                    ToolId = table.Column<int>(type: "int", nullable: false),
+                    TagId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ToolTags", x => new { x.ToolId, x.TagId });
+                    table.ForeignKey(
+                        name: "FK_ToolTags_Tags_TagId",
+                        column: x => x.TagId,
+                        principalTable: "Tags",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ToolTags_Tools_ToolId",
+                        column: x => x.ToolId,
+                        principalTable: "Tools",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TagTool_ToolsId",
+                table: "TagTool",
+                column: "ToolsId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ToolTags_TagId",
+                table: "ToolTags",
+                column: "TagId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TagTool");
+
+            migrationBuilder.DropTable(
+                name: "ToolTags");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ToolId",
+                table: "Tags",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tags_ToolId",
+                table: "Tags",
+                column: "ToolId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tags_Tools_ToolId",
+                table: "Tags",
+                column: "ToolId",
+                principalTable: "Tools",
+                principalColumn: "Id");
+        }
+    }
+}
